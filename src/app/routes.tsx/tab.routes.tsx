1833b9d5966dd78599ceb0205ff6aac3e8e45b4a
Rafaela Narcizo <narcizorafaela@gmail.com>
2026-03-01 16:20:22 -0300

    ajustes tabbar

--- src/app/routes.tsx/tab.routes.tsx
+++ src/app/routes.tsx/tab.routes.tsx
@@ -1,18 +1,20 @@
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AnonymousLogin from "../screens/anonymouslogin";
-import Login from "../screens/login";
+import AnimalReport from "../screens/animalreport";
 import Maps from "../screens/maps";
-import Signup from "../screens/signup";
+import Profile from "../screens/profile";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabRoutes() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Maps" component={Maps} />
-      <Tab.Screen name="Login" component={Login} />
-      <Tab.Screen name="Signup" component={Signup} />
-      <Tab.Screen name="AnonymousLogin" component={AnonymousLogin} />
+      <Tab.Screen name="Mapa" 
+      component={Maps} 
+      options={{tabBarIcon: ({size, color}) => <Feather name="home" size={size}  color={color}/>}} />
+      <Tab.Screen name="Relatar Animal" component={AnimalReport} options={{tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="dog" size={size} color={color} />}} />
+      <Tab.Screen name="Perfil" component={Profile} options={{tabBarIcon: ({size, color}) => <Feather name="user" size={size} color={color} />}} />
+      
     </Tab.Navigator>
     
   );
